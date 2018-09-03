282f49a6d52402fa2be836b7e5359a624b53a6bd
Lucas Augusto <lucas.tansini@hotmail.com>
2018-09-03 12:03:18 -0300

    Arithmetic Instructions and small bufix
    Bugfix caused by same masks matching more than one instruction because we iterate on an array of masks. 
    Output formatting

--- src/instMonitor.c
+++ src/instMonitor.c
@@ -15,6 +15,7 @@
 #define STORE_COUNTER 0
 #define LOAD_COUNTER 1
 #define BRANCH_COUNTER 2
+#define ARITHMETIC_COUNTER 3
 //.
 //.
 //Add more counters here
@@ -28,7 +29,7 @@
 
 //Change the lenght of the array when you add more masks.
 //TODO maybe making this modular
-#define MASK_ARRAY_LENGTH 1
+
 #define MATCH_SB 0x23 	 //Store byte
 #define MATCH_SH 0x1023  //Store half word
 #define MATCH_SW 0x2023  //Store word
@@ -40,7 +41,48 @@
 #define MATCH_LBU 0x4003
 #define MATCH_LHU 0x5003
 #define MATCH_LWU 0x6003
-#define MASK_STORE_LOAD 0x707f  //Store and load have the same mask
+#define MATCH_BEQ 0x63
+#define MATCH_BNE 0x1063
+#define MATCH_BLT 0x4063
+#define MATCH_BGE 0x5063
+#define MATCH_BLTU 0x6063
+#define MATCH_BGEU 0x7063
+#define MATCH_JALR 0x67
+#define MATCH_JAL 0x6f
+#define MATCH_ADDI 0x13 //Mask is same of SW,LW,etc..
+#define MATCH_SLLI 0x1013
+#define MATCH_SLTI 0x2013
+#define MATCH_SLTIU 0x3013
+#define MATCH_XORI 0x4013
+#define MATCH_SRLI 0x5013
+#define MATCH_SRAI 0x40005013
+#define MATCH_ORI 0x6013
+#define MATCH_ANDI 0x7013
+#define MATCH_ADD 0x33
+#define MATCH_SUB 0x40000033
+#define MATCH_SLL 0x1033
+#define MATCH_SLT 0x2033
+#define MATCH_SLTU 0x3033
+#define MATCH_XOR 0x4033
+#define MATCH_SRL 0x5033
+#define MATCH_SRA 0x40005033
+#define MATCH_OR 0x6033
+#define MATCH_AND 0x7033
+#define MATCH_ADDIW 0x1b
+#define MATCH_SLLIW 0x101b
+#define MATCH_SRLIW 0x501b
+#define MATCH_SRAIW 0x4000501b
+#define MATCH_ADDW 0x3b
+#define MATCH_SUBW 0x4000003b
+#define MATCH_SRLW 0x503b
+#define MATCH_SRAW 0x4000503b
+
+//Masks
+#define MASK_ARRAY_LENGTH 5
+#define MASK_JAL 0x7f
+#define MASK_ADD  0xfe00707f
+#define MASK_SLLI  0xfc00707f //SRLI,SRAI have the same mask.
+#define MASK_STORE_LOAD_BRANCH 0x707f  //Stores, loads and branches  have the same mask
 
 
 
@@ -66,7 +108,11 @@ int main(int argc, char const *argv[]) {
   unsigned int instruction;
 
   /*Array of instruction masks*/
-  unsigned int maskArray[MASK_ARRAY_LENGTH] = {MASK_STORE_LOAD};
+  unsigned int maskArray[MASK_ARRAY_LENGTH] = {MASK_STORE_LOAD_BRANCH,
+                                              MASK_JAL,
+                                              MASK_SLLI,
+                                              MASK_JAL,
+                                              MASK_ADD};
   //Variable that stores the result of the AND operation between instruction and mask.
   unsigned int match;
 
@@ -123,7 +169,9 @@ int main(int argc, char const *argv[]) {
         */
         //Convert the string to integer
         unsigned int mainPCnumber = (int)strtol(mainPC,NULL,16);
-        long int counterline = 0;
+        // long int counterline = 0;
+
+        int i;
 
         while(!feof(commitFile)){
           fscanf(commitFile,"%d %x (%x)",&first,&programCounter,&instruction);
@@ -138,39 +186,92 @@ int main(int argc, char const *argv[]) {
           }
 
           if(beginCount){
-            counterline++;
-            int i;
+            // counterline++;
+
 
             //printf("\nInstruction hex:%x\n",instruction);
+            int maskFound = 0;
 
-            for(i = 0; i < MASK_ARRAY_LENGTH ; i++){
-              //Perfoming an AND operation to match the instruction.
-              match = instruction & maskArray[i];
-              //See what instruction match we got.
-
-              switch (match) {
-                case MATCH_SB:
-                case MATCH_SD:
-                case MATCH_SH:
-                case MATCH_SW:{
+            //Each instruction can only have one mask match at a time.
+              for(i = 0; i < MASK_ARRAY_LENGTH ; i++){
+                //Perfoming an AND operation to match the instruction.
+                if(!maskFound){
+                  match = instruction & maskArray[i];
+                  //See what instruction match we got.
                   // printf("\nInstruction hex:%x\n",instruction);
                   // printf("\nMask analized:%x\n",maskArray[i]);
                   // printf("\nMatch result%x\n",match);
-                  // getchar();
-                  counter[STORE_COUNTER]++;
+                  switch (match) {
+                    case MATCH_SB:
+                    case MATCH_SD:
+                    case MATCH_SH:
+                    case MATCH_SW:{
+                      maskFound = 1;
+                      counter[STORE_COUNTER]++;
+                    }
+                    break;
+                    case MATCH_LB:
+                    case MATCH_LD:
+                    case MATCH_LH:
+                    case MATCH_LW:
+                    case MATCH_LBU:
+                    case MATCH_LHU:
+                    case MATCH_LWU:{
+                      maskFound = 1;
+                      counter[LOAD_COUNTER]++;
+                    }
+                    break;
+                    case MATCH_BEQ:
+                    case MATCH_BNE:
+                    case MATCH_BLT:
+                    case MATCH_BGE:
+                    case MATCH_BLTU:
+                    case MATCH_BGEU:
+                    case MATCH_JALR:
+                    case MATCH_JAL:{
+                      maskFound = 1;
+                      counter[BRANCH_COUNTER]++;
+                    }
+                    break;
+                    case MATCH_ADDI:
+                    case MATCH_SLLI:
+                    case MATCH_SLTI:
+                    case MATCH_SLTIU:
+                    case MATCH_XORI:
+                    case MATCH_SRLI:
+                    case MATCH_SRAI:
+                    case MATCH_ORI:
+                    case MATCH_ANDI:
+                    case MATCH_ADD:
+                    case MATCH_SUB:
+                    case MATCH_SLL:
+                    case MATCH_SLT:
+                    case MATCH_SLTU:
+                    case MATCH_XOR:
+                    case MATCH_SRL:
+                    case MATCH_SRA:
+                    case MATCH_OR:
+                    case MATCH_AND:
+                    case MATCH_ADDIW:
+                    case MATCH_SLLIW:
+                    case MATCH_SRLIW:
+                    case MATCH_SRAIW:
+                    case MATCH_ADDW:
+                    case MATCH_SUBW:
+                    case MATCH_SRLW:
+                    case MATCH_SRAW:{
+                      maskFound = 1;
+                      counter[ARITHMETIC_COUNTER]++;
+                    }break;
+                    default:{
+
+                    }break;
+                  }
                 }
-                break;
-                case MATCH_LB:
-                case MATCH_LD:
-                case MATCH_LH:
-                case MATCH_LW:
-                case MATCH_LBU:
-                case MATCH_LHU:
-                case MATCH_LWU:{
-                  counter[LOAD_COUNTER]++;
-                }
+                // printf("Mask found:%d\n",maskFound);
+                // getchar();
               }
-            }
+
           }
           //We went untill the first RET instruction inside main.
           if((instruction == INSTRUCTION_RET)){
@@ -180,10 +281,12 @@ int main(int argc, char const *argv[]) {
         //TODO maybe treat the vector as an object ? kinda nice
         //Show the results
         printf("\\--------------------------------------\\\n");
-        printf("\tSTORE: %d\n",counter[STORE_COUNTER]);
-        printf("\tLOAD: %d\n",counter[LOAD_COUNTER]);
+        printf("\tSTORE:\t\t%d\n",counter[STORE_COUNTER]);
+        printf("\tLOAD:\t\t%d\n",counter[LOAD_COUNTER]);
+        printf("\tBRANCHES:\t%d\n",counter[BRANCH_COUNTER]);
+        printf("\tARITHMETIC:\t%d\n",counter[ARITHMETIC_COUNTER]);
         printf("\\--------------------------------------\\\n");
-        printf("lines from main:%ld\n",counterline);
+        // printf("lines from main:%ld\n",counterline);
       }
     }
   }
